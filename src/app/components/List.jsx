52c1c691eaccea22795e68934334a7d4b7e9c3fd
Fábio-Mauricio <fabio.mauricio.dev@gmail.com>
2025-09-24 13:12:04 -0300

    Primeiro commit

---
+++ src/app/components/List.jsx
@@ -0,0 +1,39 @@
+export const List = ({}) => {
+    return (
+        <ul className="list rounded-box shadow-md text-slate-100 flex flex-col gap-5 uppercase font-bold">
+  
+  <li className="list-row flex items-center">
+    <div className="text-7xl font-thin opacity-30 tabular-nums">01</div>
+    <div className="list-col-grow">
+      <div>Organize</div>
+     
+    </div>
+    <button className="btn btn-square btn-ghost">
+    </button>
+  </li>
+  
+  <li className="list-row flex items-center">
+    <div className="text-7xl font-thin opacity-30 tabular-nums">02</div>
+   
+    <div className="list-col-grow">
+      <div>Faça</div>
+      
+    </div>
+    <button className="btn btn-square btn-ghost">
+    </button>
+  </li>
+  
+  <li className="list-row flex items-center">
+    <div className="text-7xl font-thin opacity-30 tabular-nums">03</div>
+    
+    <div className="list-col-grow">
+      <div>Evolua</div>
+    
+    </div>
+    <button className="btn btn-square btn-ghost">
+    </button>
+  </li>
+  
+</ul>
+    )
+}
